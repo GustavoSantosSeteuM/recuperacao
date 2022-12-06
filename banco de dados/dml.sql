b11629f886a4a5a96515cdbb072aab9029bdc7d1
GustavoSantosSeyeuM <gl4040123@gmail.com>
2022-12-06 17:55:04 -0300

    recuperacao ;-;

---
+++ banco de dados/dml.sql
@@ -0,0 +1,9 @@
+USE recuperacaoDB;
+
+INSERT INTO tb_aluno(nm_aluno, vl_chamada, ds_turma)
+				VALUES('pedrinho','1','A');
+
+
+--LISTAR TODOS OS ALUNOS
+
+SELECT * FROM tb_aluno;
---
+++ site/src/Routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+
+export default function index(){
+    return(
+        <BrowserRouter>
+            <Routes>
+             <Route path=''/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
